cc94191301ca4b61b20f0868d5f127e5d319de23
ok2ju <soulSpringg@gmail.com>
2017-09-10 18:53:23 +0300

    Todo routing, handlers example

--- src/main.go
+++ src/main.go
@@ -1,19 +1,54 @@
 package main
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", Hello)
-	http.Handle("/", r)
-	fmt.Println("Starting up on 8090")
-	log.Fatal(http.ListenAndServe(":8090", nil))
+type Todo struct {
+	Name string `json:"name"`
+	Completed bool `json:"completed"`
+	Due time.Time `json:"due"`
 }
 
-func Hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
+type Todos []Todo
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+	
+
+	fmt.Println("Starting up on 8090")
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(todos)
+
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo index")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
